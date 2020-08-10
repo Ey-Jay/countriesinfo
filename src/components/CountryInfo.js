ea3972795cc20a27057968133df6f61bf5c7a36a
Erick Jansen <janseniter@gmail.com>
2020-08-10 15:58:17 +0200

    fix empty currency bug

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -75,13 +75,16 @@ function CountryInfo({ code }) {
           </div>
         </>
       )}
-      {data.country.currency.length > 0 && (
+      {data.country.currency && data.country.currency.length > 0 && (
         <p style={{ padding: '0 20px' }}>
           if you want to go shopping, <i>make sure</i> you have some{' '}
           <b>{data.country.currency}</b> on you. That is the widely used
           currency.
         </p>
       )}
+      {/* {data.country.currency.length > 0 && (
+        
+      )} */}
     </>
   );
 }
